395da320b56cf1c3a0f0fc8fceb34be8b6a15c30
Ash <ash.tracdelight@gmail.com>
2016-09-27 22:45:44 +0200

    adding more dependencies

--- installDeps.sh
+++ installDeps.sh
@@ -2,5 +2,6 @@ set -e
 sudo apt-get update
 sudo apt-get install libmysqlclient-dev -y
 sudo apt-get install python-dev -y
-sudo apt-get install pip
+sudo apt-get install python-setuptools
+sudo easy_install pip
 sudo pip install -r requirements.txt
