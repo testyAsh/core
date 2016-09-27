96dd0185fdff813ccae9b3218135c65cfc6d52ad
Ash <ash.tracdelight@gmail.com>
2016-09-27 22:47:15 +0200

    adding -y to dependency install

--- installDeps.sh
+++ installDeps.sh
@@ -2,6 +2,6 @@ set -e
 sudo apt-get update
 sudo apt-get install libmysqlclient-dev -y
 sudo apt-get install python-dev -y
-sudo apt-get install python-setuptools
+sudo apt-get install python-setuptools -y
 sudo easy_install pip
 sudo pip install -r requirements.txt
