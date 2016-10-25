9b37a27f252918515cc4cbcb118a672566fd173c
cromulon <gharbi.wafa18@gmail.com>
2016-10-25 19:26:36 +0100

    modified addworker in views.py, data successfully sent but not added in DB

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request, json
 from app import app, db
 import MySQLdb
 
@@ -112,19 +112,29 @@ def addWorker():
 
 @app.route('/addNewWorker', methods=['POST', 'GET'])
 def addNewworker():
-    firstname = request.form['Nom'];
-    lastname = request.form['Prenom'];
-    phonenumber = request.form['Numero'];
-    legalid = request.form['CIN'];
-    address = request.form['Addresse'];
-    retrievalrule = request.form['Recouvrement'];
-    comments = request.form['Commentaires'];
     db, cur = connectDb()
-    cur.execute("INSERT INTO Workers (FirstName,LastName,PhoneNumber,LegalID,Address,RetrievalRule,Comments) VALUES (%s,%s,%s,%s,%s,%s,%s)",(firstName,lastName,phoneNumber,legalID,address,retrievalRule,comments))
-    return json.dumps({'status':'OK','worker':firstname});
-    # return redirect('/workers/get_workers')
-    db.close()
+    if request.method=='POST':
+     firstname = request.form['Nom']
+     lastname = request.form['Prenom']
+     phonenumber = request.form['Numero']
+     legalid = request.form['CIN']
+     address = request.form['Addresse']
+     retrievalrule = request.form['Recouvrement']
+     comments = request.form['Commentaires']
+     print firstname,lastname,phonenumber,legalid,address,retrievalrule,comments
+    try:
 
+      with cur:
+        fields = (firstname,lastname,phonenumber,legalid,address,retrievalrule,comments)
+        query = """INSERT INTO Workers (FirstName,LastName,PhoneNumber,LegalID,Address,RetrievalRule,Comments) VALUES (%s,%s,%s,%s,%s,%s,%s);"""
+        cur.execute(query, fields)
+        db.commit()
+    finally:
+     db.close()
+     return json.dumps({"result":"Saved successfully."})
+    # return json.dumps({'status':'OK','worker':firstname});
+    # return redirect('/workers/get_workers')
+  
 
 # @app.route('/workerJobs')
 # def workersJobs():
