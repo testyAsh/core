dd43117e95084e7e96916dc8a2c650b69406da3a
Ash <ash.tracdelight@gmail.com>
2016-12-02 18:45:34 +0100

    beautifying recap

--- app/static/js/createorder5.js
+++ app/static/js/createorder5.js
@@ -13,15 +13,18 @@ function fetchrecap() {
 function createrecap(data){
     var i;
     var html = '';
-    html += '<a href="#" class="list-group-item">Client : '+ data["Clientprenom"] + ' '+ data["clientnom"] +'</a>'
-    html += '<a href="#" class="list-group-item">Téléphone client : '+ data["telclient"] +'</a>'
-    html += '<a href="#" class="list-group-item">Adresse client : '+ data["Adressclient"] +'</a>'
+    html += '<a href="#" class="list-group-item list-group-item-info">Client</a>'
+    html += '<a href="#" class="list-group-item">Nom : '+ data["Clientprenom"] + ' '+ data["clientnom"] +'</a>'
+    html += '<a href="#" class="list-group-item">Téléphone : '+ data["telclient"] +'</a>'
+    html += '<a href="#" class="list-group-item">Adresse : '+ data["Adressclient"] +'</a>'
     html += '<a href="#" class="list-group-item">Moyen de contact : '+ data["ContactWay"] +'</a>'
-    html += '<a href="#" class="list-group-item">Service : '+ data["Service"] +'</a>'
-    html += '<a href="#" class="list-group-item">Ouvrier : '+ data["Workerprenom"] + ' '+ data["Workernom"] +'</a>'
-    html += '<a href="#" class="list-group-item">Téléphone ouvrier : '+ data["Workertel"] +'</a>'
+    html += '<a href="#" class="list-group-item list-group-item-success">Service</a>'
+    html += '<a href="#" class="list-group-item">Type : '+ data["Service"] +'</a>'
     html += '<a href="#" class="list-group-item">Date de contact : '+ data["ContactDate"] +'</a>'
     html += '<a href="#" class="list-group-item">Date d\'execution : '+ data["ExecutionDate"] +'</a>'
+    html += '<a href="#" class="list-group-item list-group-item-warning">Ouvrier</a>'
+    html += '<a href="#" class="list-group-item">Nom : '+ data["Workerprenom"] + ' '+ data["Workernom"] +'</a>'
+    html += '<a href="#" class="list-group-item">Téléphone : '+ data["Workertel"] +'</a>'
    
     
  
